81774218d9edbc4b341bcbc652c455d85e34eafb
Weverton-profile <wevertonalves.profile@gmail.com>
2023-03-12 22:06:37 -0300

    correcting routes

--- src/main/java/br/com/digidex/controller/DigimonController.java
+++ src/main/java/br/com/digidex/controller/DigimonController.java
@@ -3,6 +3,7 @@ package br.com.digidex.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,9 +23,21 @@ public class DigimonController {
 	
 	@GetMapping("/digimon") 
 	public String digimon(@RequestParam("name") String name, Model model) {
+		if (name.isBlank()) {
+			model.addAttribute("message", "fill in the field so we can search.");
+			return "home";
+		}
 		Digimon digimon = digimonService.encontrarDigimonPorNome(name);
+		if (digimon.getId() == null) {
+			model.addAttribute("message", "The informed digimon was not found.");
+			return "home";
+		}
 		model.addAttribute("digimon", digimon);
 		return "digimon";
 	}
 	
+	@ExceptionHandler(Exception.class)
+	public String onError() {
+		return "redirect:/home";
+	}
 }
