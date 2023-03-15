1008c4b3fb06bc72381d1d6ac0bbe51e1be2f118
Weverton-profile <wevertonalves.profile@gmail.com>
2023-03-15 20:21:12 -0300

    finished v1.0

--- src/main/java/br/com/digidex/controller/DigimonController.java
+++ src/main/java/br/com/digidex/controller/DigimonController.java
@@ -3,7 +3,6 @@ package br.com.digidex.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,7 +16,8 @@ public class DigimonController {
 	DigimonService digimonService;
 
 	@GetMapping("/home")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("message", "Digimon name");
 		return "home";
 	}
 	
@@ -36,8 +36,4 @@ public class DigimonController {
 		return "digimon";
 	}
 	
-	@ExceptionHandler(Exception.class)
-	public String onError() {
-		return "redirect:/home";
-	}
 }
